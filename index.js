8c871a5cac2d87d23f849e31fd943af8fded550e
Eric Van <ggclarence@yahoo.com>
2022-02-04 20:52:17 -0600

    commit tree e62e04960088cfc1e35b4836fe5acf6d531c50b2

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 // Write your solution in this file!
-var employee = {}
+// var employee = {}
 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObj = {...employee};
-    newObj[key] = value;
-    return newObj;
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newObj = {...employee};
+//     newObj[key] = value;
+//     return newObj;
+// }
+
+//  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//         employee[key] = value;
+//         return employee;
+//        }
+// function deleteFromEmployeeByKey(employee, key){
+//     const newemployee = {...employee}
+//     delete newemployee[key]
+//     return newemployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key]
+//     return employee
+//}
+
+
+const employee = {}
+
+function updateEmployeeWithKeyAndValue (employee,key,value){
+  return{ 
+      ...employee,
+ 
+     [key]:value,
+  };
+     
 }
 
- function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-        employee[key] = value;
-        return employee;
-       }
-function deleteFromEmployeeByKey(employee, key){
-    const newemployee = {...employee}
-    delete newemployee[key]
-    return newemployee
+function destructivelyUpdateEmployeeWithKeyAndValue (obj,key,value){
+   obj[key] = value
+   return obj
+}
+
+function deleteFromEmployeeByKey(employee,key){
+ const newEmpolyee = {...employee}
+ delete newEmpolyee[key]
+ return newEmpolyee
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key]
     return employee
 }
+
+
